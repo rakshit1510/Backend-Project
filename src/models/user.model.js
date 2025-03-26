841f480dea74b25837efce1273651245066c0b46
rakshit1510 <gargrakshit10@gmail.com>
2025-03-26 21:56:42 +0530

    user and video model made and token and bcrypt used

---
+++ src/models/user.model.js
@@ -0,0 +1,99 @@
+import mongoose from 'mongoose'
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
+const userSchema=new mongoose.Schema(
+    {
+        username:{
+            type:String,
+            required:true,
+            lowercase:true,
+            unique:true,
+            trim:true,
+            index:true,
+        },
+        email:{
+            type:String,
+            required:true,
+            lowercase:true,
+            unique:true,
+            trim:true,
+        },
+        fullname:{
+            type:String,
+            required:true,
+            trim:true,
+            index:true,
+        },
+        avatar:{
+            type:String, //cloudinary url
+            required:true,
+        },
+        coverImage:{
+            type:String,
+        },
+        watchHistory:[
+            {
+                type:mongoose.Schema.Types.ObjectId,
+                ref:"Video"
+            }
+        ],
+        password:{
+            type:String,
+            required:[true,'Password is required']
+        },
+        refreshToken:{
+            type:String,
+        }
+},{timestamps : true })
+
+//next is a callback function used in Mongoose middleware (hooks).
+//  It is used to pass control to the next middleware or finish the execution of the current middleware.
+
+
+/*
+
+pre is a Mongoose middleware (hook) that runs before a certain action (like saving or updating a document).
+
+It allows us to modify data, validate fields, or perform actions before saving, updating, or deleting a document in MongoDB.
+
+*/
+userSchema.pre("save",async function(next){
+    if(!this.isModified("password"))return next();
+    this.password= bcrypt.hash(this.password,10)
+    next();
+})
+
+// "methods" is a hook in mongoose which
+//  allows you to made your own middlewares
+
+userSchema.methods.isPasswordCorrect=async function(password){
+    return await bcrypt.compare(password,this.password)
+}
+
+userSchema.methods.generateAccessToken= function(){
+    return jwt.sign(
+        {
+            _id:this._id,
+            email:this.email,
+            username:this.username,
+            fullname:this.fullname,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken= function(){
+    return jwt.sign(
+        {
+            _id : this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+}
+export const User= mongoose.model("User",userSchema)
